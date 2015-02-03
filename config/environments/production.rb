6c92438dd992b28dbf2d14bb08164bb711af6273
Tjerk Dijkstra <tjerk.dijkstra@gmail.com>
2015-02-03 13:57:17 +0100

    smtp settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,15 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'mail.studentschema.nl',
+    port:                 587,
+    domain:               'studentschema.nl',
+    user_name:            'info@studentschema.nl',
+    password:             '4teNKoDq',
+    authentication:       'plain',
+    enable_starttls_auto: true  }
+
 end
